2acb6309004db2208cb1e492124abfa008527911
Arjunssolanki <arjunsingh.eee11@gmail.com>
2024-03-21 18:10:16 +0530

    Logging and exception

--- template.py
+++ template.py
@@ -8,6 +8,7 @@ project_name="mlproject"
 
 list_of_files=[
     ".github/workflows/.gitkeep",
+    f"src/__init__.py",
     f"src/{project_name}/__init__.py",
     f"src/{project_name}/components/__init__.py",
     f"src/{project_name}/components/data_ingestion.py",
